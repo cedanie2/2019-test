6bfc5813fe406d6bfdc3f2928cb416fcd39edd68
Clinton Daniel <cedanie2@usf.edu>
2019-08-28 23:39:13 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2019_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Another Change");
+        }
+    }
+}
